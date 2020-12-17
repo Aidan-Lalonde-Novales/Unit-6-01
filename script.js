dfb1aaadcec57aceed639450ed2c28733557563d
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2020-12-17 16:07:14 +0000

    i really suck at js, thank god i got help!

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+"use strict";
+
 // JS that randomly generates a number from 1-6 and checks if the user guessed it correctly.
 
 document.getElementById('submitbutton').addEventListener('click', guessChecker)
 
 function guessChecker () {
-  const guessedNumber = document.getElementById('userguess').value // Makes the value of the textbox into a variable
-  const rolledNumber = Math.floor(Math.random() * 6) + 1 // Uses RNG to produce a number from 1-6
+  let rolledNumber = Math.floor(Math.random() * 6) + 1 // Uses RNG to produce a number from 1-6
+  let guessedNumber = document.getElementById('userguess').value // Makes the value of the textbox into a variable
   if (guessedNumber == rolledNumber) {
-    var result = 'Correct!'
+    document.getElementById('result').innerHTML = 'Correct!'
+  } else if (guessedNumber >= 7) {
+    document.getElementById('result').innerHTML = 'Really?'
+  } else if (guessedNumber <= 0) {
+    document.getElementById('result').innerHTML = 'Really?'
   } else {
-    var result = 'Incorrect. The correct answer was ' + rolledNumber + '.'
+    document.getElementById('result').innerHTML = 'Incorrect. The correct answer was ' + rolledNumber + '.'
   }
-  document.getElementById('result').innerHTML = result // replaces a textbox with the result of the guess
 }
