91d8aa95e337ffee0b654f131c076a456b9fc2bd
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2020-12-18 17:47:27 +0000

    === error will have to stay

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-"use strict";
-
 // JS that randomly generates a number from 1-6 and checks if the user guessed it correctly.
 
 document.getElementById('submitbutton').addEventListener('click', guessChecker)
 
 function guessChecker () {
-  let rolledNumber = Math.floor(Math.random() * 6) + 1 // Uses RNG to produce a number from 1-6
-  let guessedNumber = document.getElementById('userguess').value // Makes the value of the textbox into a variable
+  const rolledNumber = Math.floor(Math.random() * 6) + 1 // Uses RNG to produce a number from 1-6
+  const guessedNumber = document.getElementById('userguess').value // Makes the value of the textbox into a variable
   if (guessedNumber == rolledNumber) {
     document.getElementById('result').innerHTML = 'Correct!'
   } else if (guessedNumber >= 7) {
